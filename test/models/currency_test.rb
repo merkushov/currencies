b30f95d63e68137cc8b246f09cf365a07a430c22
Victor Merkushov <merkushov.victor@gmail.com>
2020-01-24 14:42:36 +0300

    created auto-test for Currency model

--- test/models/currency_test.rb
+++ test/models/currency_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CurrencyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "successful save" do
+    currency = currencies(:one)
+
+    obj = Currency.new(
+      code: currency.code,
+      name: currency.name,
+      rate: currency.rate,
+      measure_date: '2000-01-01',
+    )
+    assert obj.save, "Saved successfuly"
+
+    assert_equal 1, Currency.where(
+      code: currency.code,
+      measure_date: '2000-01-01'
+    ).count, "The record is in the database"
+  end
+
+  test "should not save article without required fields" do
+    obj = Currency.new
+    assert_not obj.save, "Saved the currency without a code and name"
+  end
 end
